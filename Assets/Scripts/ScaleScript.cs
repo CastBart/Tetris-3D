447ba8b6fced5202a2ae491fb3c5259fcf8ca3b9
KDaryl <c00207452@itcarlow.ie>
2017-11-20 10:22:44 +0000

    Added block scale up effect

---
+++ Assets/Scripts/ScaleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleScript : MonoBehaviour
+{
+    float scaleSpeed = 0.03f;
+    const float maxScale = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //Make our block's scale 0
+        transform.localScale = new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (transform.localScale.x < maxScale)
+        {
+            //get the current scale of the object
+            Vector3 currentScale = transform.localScale;
+
+            transform.localScale = new Vector3(currentScale.x += scaleSpeed, currentScale.y += scaleSpeed, currentScale.z += scaleSpeed);
+
+            //if our scal eis gone past our max scale then set it to our max scale
+            if(transform.localScale.x > 1)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+        }
+	}
+}
