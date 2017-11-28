3868ebfa6c4cdabc824efe2d56ea33deb609c0c2
Davidnln13 <davidnln13@yahoo.com>
2017-11-28 21:43:47 +0000

    Added pause restart and exit functionality

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -2,12 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ButtonManager : MonoBehaviour {
-
+public class ButtonManager : MonoBehaviour
+{
+    bool paused;
+    public Text pauseText;
+    void Start()
+    {
+        pauseText.GetComponent<Text>();
+        paused = false;
+        pauseText.enabled = false;
+    }
     public void SwitchScene(string lvl)
     {
         SceneManager.LoadScene(lvl);
+    }
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void PauseGame()
+    {
+        paused = !paused;
+        if (paused == true)
+        {
+            pauseText.enabled = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseText.enabled = false;
+            Time.timeScale = 1;
+        }
     }
     public void exit()
     {
